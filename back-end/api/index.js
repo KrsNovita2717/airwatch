d353a366bdd3771e41e3114b134e4f772d5c598d
Leonardo Denavito <jounandavito@gmail.com>
2023-12-12 12:38:36 +0700

    Updated gitignore

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-const { swaggerUi, specs } = require('./swagger');
 
 const app = express();
-
-app.use('/api-docs', swaggerUi.serve);
-app.get('/api-docs', swaggerUi.setup(specs));
 
 app.use(express.json());
 
